fb226d90c2773b804325835181f8f70c119d4dfa
berfin şalk <berfinsalk@icloud.com>
2023-04-28 14:41:32 +0300

    add

---
+++ index.js
@@ -0,0 +1,16 @@
+//4 İŞLEM
+let x =10;
+let y = 5;
+ console.log("x+y= "+ (x+y));
+ console.log("x-y= "+ (x-y));
+ console.log("x*y= "+ (x*y));
+ console.log("x/y= "+ (x/y));
+ 
+ //FOR DÖNGÜSÜ
+ for(let i=0; i<=10; i++){
+  console.log(i);
+ }
+
+
+
+
